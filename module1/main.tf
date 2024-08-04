43c0317c83a2479efa8408cc8e4c3fc861d49ed8
johithkrishna <johithkrishna0110@gmail.com>
2024-08-04 22:15:48 +0530

    backend added

--- module1/main.tf
+++ module1/main.tf
@@ -5,6 +5,12 @@ terraform {
       version = "~> 5.0"
     }
   }
+  backend "s3" {
+    bucket         = "aws122check"
+    key            = "terraform/state.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+  }
 }
 
 provider "aws" {
